78c797461888a16bb990ac3c305c6d51070e2c29
anisseb <anisse71100@hotmail.fr>
2016-09-29 14:30:02 +0200

    add new template

---
+++ foot2rue/www/js_demartin/app.js
@@ -0,0 +1,108 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic', 'ionic-material']);
+
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('register', {
+        url: '/register',
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterCtrl'
+    })
+
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+    })
+
+    .state('app.lists', {
+        url: '/lists',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/lists.html',
+                controller: 'ListsCtrl'
+            }
+        }
+    })
+
+    .state('app.ink', {
+        url: '/ink',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/ink.html',
+                controller: 'InkCtrl'
+            }
+        }
+    })
+
+    .state('app.motion', {
+        url: '/motion',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/motion.html',
+                controller: 'MotionCtrl'
+            }
+        }
+    })
+
+    .state('app.profil', {
+        url: '/profil',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/profil.html',
+                controller: 'ProfilCtrl'
+            }
+        }
+    })
+
+    .state('app.profile', {
+        url: '/profile',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/profile.html',
+                controller: 'ProfileCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+    ;
+
+    // Thanks to Ben Noblet!
+  $urlRouterProvider.otherwise(function ($injector, $location) {
+    var $state = $injector.get("$state");
+    $state.go("app.profil");
+  });
+});
